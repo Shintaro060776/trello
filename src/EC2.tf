24e404277d46657e773f5f6fc45e589b4a2df83e
shin20090317 <shintaro060776@gmail.com>
2023-08-23 17:44:32 +0900

    7

--- src/EC2.tf
+++ src/EC2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "ec2_key" {
   key_name   = "ec2-ed25519-key"
-  public_key = file("/home/shintaro/.ssh/new_deploy_key.pub")
+  public_key = file("id_ed25519.pub")
 }
 
 resource "aws_instance" "example" {
